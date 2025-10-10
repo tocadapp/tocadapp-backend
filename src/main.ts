22d156d5aa8f640ceb1622533d3aa6e74c4f58e7
Luis Ángel <devjsluis@gmail.com>
2025-10-09 23:13:47 -0600

    feat: :bug: Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,11 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-      origin: ['http://localhost:3000', 'https://tocadapp.com'],
+      origin: [
+        'http://localhost:3000',
+        'https://tocadapp-landing.vercel.app/',
+        'https://www.tocadapp.com',
+      ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       credentials: true,
     });
